bdf00151d31b072527d5c4494db509258b783849
Sanish Shrestha <sanish_shr1995@hotmail.com>
2022-09-27 07:29:52 +0545

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
+
+// check compatibility
+if (!("BarcodeDetector" in window)) {
+    console.log("Barcode Detector is not supported by this browser.");
+    document.getElementById('message').textContent = "Not Working";
+  } else {
+    console.log("Barcode Detector supported!");
+  
+    // create new detector
+    const barcodeDetector = new BarcodeDetector({
+      formats: ["code_39", "codabar", "ean_13"],
+    });
+  }
