46618eca3fd5f345c05402008126469d5c0769a7
Sanish Shrestha <sanish_shr1995@hotmail.com>
2022-09-28 20:23:21 +0545

    switched to quagga

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code scanned = ${decodedText}`, decodedResult);
-    document.getElementById('message').textContent = decodedText;
-}
-var html5QrcodeScanner = new Html5QrcodeScanner(
-	"qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
 
-// check compatibility
-if (!("BarcodeDetector" in window)) {
-    console.log("Barcode Detector is not supported by this browser.");
-    document.getElementById('message').textContent = "Not Working";
-  } else {
-    console.log("Barcode Detector supported!");
-  
-    // create new detector
-    const barcodeDetector = new BarcodeDetector({
-      formats: ["code_39", "codabar", "ean_13"],
-    });
 
-    BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
-
-      var supported_formats = [];
-      supportedFormats.forEach((format) => supported_formats.push(format));
-    });
-    document.getElementById('message').textContent = supported_formats;
+Quagga.init({
+  inputStream : {
+    name : "Live",
+    type : "LiveStream",
+    // target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+  },
+  decoder : {
+    readers : ["upc_reader","upc_e_reader"]
   }
-  // check supported types
+}, function(err) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+});
