6a7637f94e68c879cb598a126dbcfc114b525a32
Sanish Shrestha <sanish_shr1995@hotmail.com>
2022-09-28 20:37:22 +0545

    testing out quagga js

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ Quagga.init({
     // target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
   },
   decoder : {
-    readers : ["upc_reader","upc_e_reader"]
+    readers : ["code_128_reader",
+    "ean_reader",
+    "ean_8_reader",
+    "code_39_reader",
+    "code_39_vin_reader",
+    "codabar_reader",
+    "upc_reader",
+    "upc_e_reader",
+    "i2of5_reader"]
   }
 }, function(err) {
     if (err) {
@@ -16,4 +24,7 @@ Quagga.init({
     }
     console.log("Initialization finished. Ready to start");
     Quagga.start();
+    Quagga.onProcessed(function(data){
+      document.getElementById('message').textContent = data.codeResult.code;
+    });
 });
